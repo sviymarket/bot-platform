7c7b95aef6ce4a2313c9a4b252061b9781ad6353
vsydor <volodymyrsy@ext.inditex.com>
2021-04-07 17:03:45 +0300

    Compiles

--- bot-platform/src/main/java/com/reconsale/bot/BotConfiguration.java
+++ bot-platform/src/main/java/com/reconsale/bot/BotConfiguration.java
@@ -1,45 +1,39 @@
 package com.reconsale.bot;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
+import com.reconsale.bot.engine.Handler;
+import com.reconsale.bot.integration.ResponseCase;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
-import com.reconsale.bot.engine.Handler;
-import com.reconsale.bot.integration.ResponseCase;
+import java.util.*;
 
 @ComponentScan("com.reconsale.bot")
 public class BotConfiguration {
-	
-	@Bean
-	public Map<String, Handler> handlers(ApplicationContext context) {
-		Map<String, Handler> beans = context.getBeansOfType(Handler.class);
-		
-		Map<String, Handler> result = new HashMap<String, Handler>();
-		
-		List<Handler> beanList = new ArrayList<>(beans.values());
-		Collections.sort(beanList, org.springframework.core.annotation.AnnotationAwareOrderComparator.INSTANCE);
-		
-		for (Handler handler : beanList) {
-			if (!result.containsKey(handler.mapping())) {
-				result.put(handler.mapping(), handler);
-			}
-		}
-		
-		return result;
-	}
 
-	@Bean
-	public List<ResponseCase<?>> responseCases(ApplicationContext context) {
-		Map<String, ResponseCase> beans = context.getBeansOfType(ResponseCase.class);
-		ArrayList<ResponseCase<?>> beanList = new ArrayList<ResponseCase<?>>((Collection<? extends ResponseCase<?>>) beans.values());
-		Collections.sort(beanList, org.springframework.core.annotation.AnnotationAwareOrderComparator.INSTANCE);
-		return beanList;
-	}
+    @Bean
+    public Map<String, Handler> handlers(ApplicationContext context) {
+        Map<String, Handler> beans = context.getBeansOfType(Handler.class);
+
+        Map<String, Handler> result = new HashMap<String, Handler>();
+
+        List<Handler> beanList = new ArrayList<>(beans.values());
+        Collections.sort(beanList, org.springframework.core.annotation.AnnotationAwareOrderComparator.INSTANCE);
+
+        for (Handler handler : beanList) {
+            if (!result.containsKey(handler.mapping())) {
+                result.put(handler.mapping(), handler);
+            }
+        }
+
+        return result;
+    }
+
+    @Bean
+    public List<ResponseCase<?>> responseCases(ApplicationContext context) {
+        Map<String, ResponseCase> beans = context.getBeansOfType(ResponseCase.class);
+        ArrayList<ResponseCase<?>> beanList = new ArrayList(beans.values());
+        Collections.sort(beanList, org.springframework.core.annotation.AnnotationAwareOrderComparator.INSTANCE);
+        return beanList;
+    }
 }
