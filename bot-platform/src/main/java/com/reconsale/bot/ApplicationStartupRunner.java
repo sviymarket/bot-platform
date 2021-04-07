aa9cc0b91520e4d03122f5869c6b55a08592d392
vsydor <volodymyrsy@ext.inditex.com>
2021-04-07 20:58:04 +0300

    Initializing viber webhook on app startup

---
+++ bot-platform/src/main/java/com/reconsale/bot/ApplicationStartupRunner.java
@@ -0,0 +1,24 @@
+package com.reconsale.bot;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+
+@Slf4j
+@Component
+public class ApplicationStartupRunner implements ApplicationRunner {
+
+    @Autowired
+    private ViberStarter viberStarter;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        log.info("Application started!");
+        log.info("Setting viber webhook...");
+        viberStarter.registerBot();
+        log.info("Webhook set!");
+    }
+}
